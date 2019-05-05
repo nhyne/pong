01f872d3365ef7d1e00949e2c0e3de3a172262fd
nhyne <adamjohnson35@gmail.com>
2019-05-05 14:26:45 -0400

    got some cleaner player stuff

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,51 @@ extern crate find_folder;
 use piston_window::draw_state::Blend;
 use piston_window::*;
 
+use math::Matrix2d;
+
+const BLACK: [f32;4] = [0.0, 0.0, 0.0, 1.0];
+const PLAYER_WIDTH: f64 = 15.0;
+const PLAYER_HEIGHT: f64 = 50.0;
+
+struct Player {
+    x_pos: f64,
+    y_pos: f64,
+    shape: Rectangle,
+}
+
+impl Player {
+
+    fn draw<G>(&self, draw_state: &DrawState, transform: Matrix2d, graphics: &mut G)
+        where G: Graphics {
+            self.shape.draw(
+                [self.x_pos, self.y_pos, PLAYER_WIDTH, PLAYER_HEIGHT],
+                draw_state,
+                transform,
+                graphics,
+            )
+        }
+
+
+    fn new(x_pos: f64, y_pos: f64) -> Player {
+        Player {
+            x_pos,
+            y_pos,
+            shape: Rectangle::new(BLACK),
+        }
+    }
+
+    fn move_up(&mut self) {
+        self.y_pos += 5.0;
+    }
+
+    fn move_down(&mut self) {
+        self.y_pos -= 5.0;
+    }
+}
+
 fn main() {
-    println!("Press A to change blending");
-    println!("Press S to change clip inside/out");
+
+    let mut player_1 = Player::new(0.0, 0.0);
 
     let mut window: PistonWindow = WindowSettings::new(
         "piston: draw_state",
@@ -19,27 +61,26 @@ fn main() {
 
     let blends = [Blend::Alpha, Blend::Add, Blend::Invert, Blend::Multiply];
     let blend = 0;
-    let x_pos = 30.0;
-    let mut y_pos = 0.0;
 
     window.set_lazy(true);
     while let Some(e) = window.next() {
-        window.draw_2d(&e, |c, g| {
-            clear([0.8, 0.8, 0.8, 1.0], g);
-            g.clear_stencil(0);
-            let cube_pos = c.transform.trans(x_pos, y_pos);
-            let draw_state = c.draw_state.blend(blends[blend]);
-            Rectangle::new([0.5, 1.0, 0.0, 0.3])
-                .draw([0.0, 0.0, 100.0, 100.0], &draw_state, cube_pos, g);
+        window.draw_2d(&e, |context, graphics| {
+            clear([0.8, 0.8, 0.8, 1.0], graphics);
+            graphics.clear_stencil(0);
+            let empty_transform = context.transform.trans(0.0, 0.0);
+            let draw_state = context.draw_state.blend(blends[blend]);
+            player_1.draw(&draw_state, empty_transform, graphics);
 
         });
 
+
+        // these things should be async, they're blocking
         if let Some(Button::Keyboard(Key::A)) = e.press_args() {
-            y_pos = y_pos - 5.0;
+            player_1.move_down()
         }
 
         if let Some(Button::Keyboard(Key::S)) = e.press_args() {
-            y_pos = y_pos + 5.0;
+           player_1.move_up()
         }
     }
 }
