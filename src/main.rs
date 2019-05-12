4a4e50ccc2f1b73d1a8b2a04d6d2a2e2f8fcdf8d
nhyne <adamjohnson35@gmail.com>
2019-05-12 15:40:08 -0400

    got the ball colliding

--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,20 @@ use math::Matrix2d;
 
 use nalgebra::{Point2, Vector2, Isometry2};
 use ncollide2d::shape::{Ball, ShapeHandle, Cuboid};
-use nphysics2d::object::{ColliderDesc, RigidBodyDesc, BodyHandle};
+use nphysics2d::object::{ColliderDesc, RigidBodyDesc, BodyHandle, BodyStatus};
 use nphysics2d::world::World;
 use nphysics2d::algebra::Velocity2;
-use core::borrow::Borrow;
+use core::borrow::{Borrow, BorrowMut};
+use nphysics2d::material::{MaterialHandle, BasicMaterial};
 
 const BLACK: [f32;4] = [0.0, 0.0, 0.0, 1.0];
 const PLAYER_WIDTH: f64 = 15.0;
 const PLAYER_HEIGHT: f64 = 50.0;
+
 const BALL_SIZE: f64 = 20.0;
+const BALL_HORIZONTAL_SPEED: f64 = 6.0;
+const BALL_VERTICAL_SPEED: f64 = 4.0;
+const BALL_SPEED: Velocity2<f64> = Velocity2::linear(BALL_HORIZONTAL_SPEED, BALL_VERTICAL_SPEED);
 
 struct Game {
     world: World<f64>,
@@ -36,12 +41,15 @@ impl Game {
     }
 
     fn init(&mut self) {
+        //TODO Write a ball init function specifically
         let ball_shape = ShapeHandle::new(Ball::new(BALL_SIZE));
-        let ball_collider = ColliderDesc::new(ball_shape);
+        let ball_collider = ColliderDesc::new(ball_shape)
+            .density(1.0)
+            .material(MaterialHandle::new(BasicMaterial::new(2.0, 0.0)));
 
         let mut rb_desc = RigidBodyDesc::new()
             .collider(&ball_collider)
-            .velocity(Velocity2::linear(3.0, 2.0));
+            .velocity(BALL_SPEED);
 
         let rigid_body = rb_desc.build(&mut self.world);
         let ball_handle = rigid_body.handle();
@@ -49,6 +57,7 @@ impl Game {
         let ball = PongBall::new(ball_handle);
         self.ball.push(ball);
 
+        //TODO Write a player init function specifically
         let player_shape = ShapeHandle::new(Cuboid::new(Vector2::repeat(3.0)));
         let player_collider = ColliderDesc::new(player_shape);
         let mut player_one_rb_desc = RigidBodyDesc::new()
@@ -59,11 +68,13 @@ impl Game {
 
         let player_one_rigid_body = player_one_rb_desc
             .position(Isometry2::translation(100.0, 100.0))
+            .status(BodyStatus::Kinematic)
             .build(&mut self.world);
         let player_one_handle = player_one_rigid_body.handle();
 
         let player_two_rigid_body = player_two_rb_desc
-            .position(Isometry2::translation(300.0, 300.0))
+            .position(Isometry2::translation(300.0, 200.0))
+            .status(BodyStatus::Kinematic)
             .build(&mut self.world);
         let player_two_handle = player_two_rigid_body.handle();
 
@@ -124,13 +135,29 @@ impl PongPlayer {
         }
     }
 
-    //fn move_up(&mut self) {
-    //    self.y_pos += 5.0;
-    //}
+    fn move_up(&mut self, world: &mut World<f64>) {
+        let player_body = world.rigid_body_mut(self.body);
+        match player_body {
+            None => {},
+            Some(b) => {
+                let player_body = b.borrow_mut();
+                let current_pos = b.position().translation.vector;
+                player_body.set_position(Isometry2::translation(current_pos[0], current_pos[1] + 5.0))
+            }
+        }
+    }
 
-    //fn move_down(&mut self) {
-    //    self.y_pos -= 5.0;
-    //}
+    fn move_down(&mut self, world: &mut World<f64>) {
+        let player_body = world.rigid_body_mut(self.body);
+        match player_body {
+            None => {},
+            Some(b) => {
+                let player_body = b.borrow_mut();
+                let current_pos = b.position().translation.vector;
+                player_body.set_position(Isometry2::translation(current_pos[0], current_pos[1] - 5.0))
+            }
+        }
+    }
 }
 
 struct PongBall {
@@ -166,11 +193,6 @@ impl PongBall {
 }
 
 fn main() {
-
-    //let mut player_1 = Player::new(50.0, 380.0);
-    //let mut player_2 = Player::new(750.0, 380.0);
-
-
     let mut game = Game::new();
     game.init();
 
@@ -184,13 +206,13 @@ fn main() {
         .unwrap();
 
     while let Some(e) = window.next() {
-        // these things should be async, they're blocking
+        // TODO Make these functions async because they're blocking
         //if let Some(Button::Keyboard(Key::A)) = e.press_args() {
-        //    player_1.move_down()
+        //    player_1.move_down(&mut game.world)
         //}
 
         //if let Some(Button::Keyboard(Key::S)) = e.press_args() {
-        //    player_1.move_up()
+        //    player_1.move_up(&mut game.world)
         //}
 
         //if let Some(Button::Keyboard(Key::K)) = e.press_args() {
@@ -200,14 +222,10 @@ fn main() {
         //    player_2.move_down()
         //}
 
-        //println!("{}", game.world.timestep());
-
         game.update();
 
         window.draw_2d(&e, |context, graphics| {
             game.render(context, graphics);
-            //player_1.draw(&context.draw_state, context.transform, graphics);
-            //player_2.draw(&context.draw_state, context.transform, graphics);
         });
 
     }
